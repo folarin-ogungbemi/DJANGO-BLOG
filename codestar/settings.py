434facda7473d68f002e8155278f63ced22a37e2
Folarin Ogungbemi <Folarine1@gmail.com>
2022-11-01 23:22:15 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -35,13 +35,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'blog'
 ]
 
 MIDDLEWARE = [
